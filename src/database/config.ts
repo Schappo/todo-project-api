43c25ba2abfa5b1a3d322b0b29740358beb679c9
Felipe Schappo <felipe.schappo@crssystem.com.br>
2020-09-05 02:13:03 -0300

    Connect Mongo

---
+++ src/database/config.ts
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const {
+  DB_URI
+} = process.env;
+
+const options = {
+  useNewUrlParser: true,
+  reconnectTries: Number.MAX_VALUE,
+  reconnectInterval: 500,
+  connectTimeoutMS: 10000,
+};
+
+mongoose.connect(DB_URI, options).then(() => {
+  console.log('MongoDb os connected!');
+})
+  .catch((err) => {
+    console.log(err)
+  })
